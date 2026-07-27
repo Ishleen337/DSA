fd472a499e867dd634f6c5b4d424852fe23f7a87
Ishleen Kaur <ishleenk722@gmail.com>
2026-07-27 16:21:01 +0530

    Added DSA solutions

---
+++ firstoccurrence.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    public int firstoccurrence(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;   // Search on left side
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public int lastOccurrence(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;    // Search on right side
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int first = firstoccurrence(nums, target);
+        int last = lastOccurrence(nums, target);
+
+        return new int[]{first, last};
+    }
+}
